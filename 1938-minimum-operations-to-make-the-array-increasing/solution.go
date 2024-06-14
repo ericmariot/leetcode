f5e2a4a716216276f37d52920343f3270b95b75c
ericmariots <ericmariots@gmail.com>
2024-06-14 12:04:05 +0000

    [LeetCode Sync] Runtime - 10 ms (62.69%), Memory - 5.3 MB (58.21%)

---
+++ 1938-minimum-operations-to-make-the-array-increasing/solution.go
@@ -0,0 +1,12 @@
+func minOperations(nums []int) int {
+    count := 0
+
+    for i := 1; i < len(nums); i++ {
+        if nums[i] <= nums[i-1] {
+            count += nums[i-1] - nums[i] + 1
+            nums[i] = nums[i-1] + 1
+        }
+    }
+
+    return count
+}
