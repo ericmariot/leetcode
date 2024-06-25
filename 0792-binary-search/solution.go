beb716290bc635f97b4dc76d337ec19803cd7a76
ericmariots <ericmariots@gmail.com>
2024-06-25 16:14:24 +0000

    [LeetCode Sync] Runtime - 27 ms (45.72%), Memory - 7.1 MB (39.13%)

--- 0792-binary-search/solution.go
+++ 0792-binary-search/solution.go
@@ -1,7 +1,20 @@
 func search(nums []int, target int) int {
-    for i, num := range nums {
-        if num == target {
-            return i
+    left, right := 0, len(nums)-1
+    
+
+    for left <= right {
+        mid := (left+right)/2
+        
+        if nums[mid] == target {
+            return mid
+        }
+
+        if nums[mid] < target {
+            left = mid+1
+        }
+
+        if nums[mid] > target {
+            right = mid-1
         }
     }
 
