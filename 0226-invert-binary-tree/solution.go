88f77c8e5ee54c25950df60bf72680f6bfd67462
ericmariots <ericmariots@gmail.com>
2024-07-22 16:15:58 +0000

    [LeetCode Sync] Runtime - 1 ms (75.00%), Memory - 2.4 MB (51.73%)

---
+++ 0226-invert-binary-tree/solution.go
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func invertTree(root *TreeNode) *TreeNode {
+    if root == nil {
+        return root
+    }
+
+    aux := root.Left
+    root.Left = root.Right
+    root.Right = aux
+    invertTree(root.Left)
+    invertTree(root.Right)
+
+    return root
+}
+
