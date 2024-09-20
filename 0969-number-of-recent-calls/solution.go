8394123588be803d9b5107c8ad3b12785ad8f076
ericmariots <ericmariots@gmail.com>
2024-09-20 20:05:51 +0000

    [LeetCode Sync] Runtime - 108 ms (67.22%), Memory - 7.9 MB (80.77%)

---
+++ 0969-number-of-recent-calls/solution.go
@@ -0,0 +1,26 @@
+type RecentCounter struct {
+    requests []int
+}
+
+
+func Constructor() RecentCounter {
+    return RecentCounter{}
+}
+
+
+func (this *RecentCounter) Ping(t int) int {
+    this.requests = append(this.requests, t)
+
+    for this.requests[0] < t-3000 {
+        this.requests = this.requests[1:]
+    }
+
+    return len(this.requests)
+}
+
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * obj := Constructor();
+ * param_1 := obj.Ping(t);
+ */
